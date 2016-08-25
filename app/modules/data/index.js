287302df3a6837ebfeced988a12ddd59a732ce14
Quinton Pike <qrpike@gmail.com>
2016-08-25 01:42:08 -0400

    Update 2016-08-25T05:42:05.434Z

--- app/modules/data/index.js
+++
@@ -1,8 +0,0 @@
-// data modules components:
-
-var service = require('./services/data.service.js');
-
-
-
-module.exports = angular.module('app.data', [])
-	.service('dataService', [ '$http', '$state', service ]);
--- app/modules/nav/index.js
+++
@@ -1,8 +0,0 @@
-// nav modules components:
-
-const controller = require('./controllers/nav.ctrl.js')
-const directive = require('./directives/nav.directive.js')
-
-module.exports = angular.module('app.nav', [] )
-	.controller( 'navCtrl', [ '$scope', '$state', 'dataService', controller ] )
-	.directive( 'navbar', [ directive ] )
